ece30abed1085ea1a05fee3616159c04317e5518
Danielle Trujillo <danielle4coding@gmail.com>
2024-09-10 10:29:03 +0200

    remove files

--- src/App.vue
+++ src/App.vue
@@ -5,15 +5,9 @@ import TheWelcome from './components/boilerplate/TheWelcome.vue'
 <template>
   <header>
     <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
   </header>
 
-  <main>
-    <TheWelcome />
-  </main>
+  <main></main>
   <router-view />
 </template>
 
