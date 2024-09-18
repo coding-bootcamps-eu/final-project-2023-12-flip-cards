082bc678f70ede314e78f00f837815c1a8aa7317
Danielle Trujillo <danielle4coding@gmail.com>
2024-09-18 07:43:31 +0200

    #12: chore: remove double code and restructure template

--- src/App.vue
+++ src/App.vue
@@ -1,53 +1,31 @@
 <template>
   <div id="app">
-    <router-view />
-    <!-- Hier wird der Inhalt der Route angezeigt -->
+    <!-- Container für die gesamte App -->
+    <LogoComponent />
+    <main>
+      <router-view />
+      <!-- Hier wird der Inhalt der Route angezeigt -->
+    </main>
   </div>
 </template>
 
 <script>
+import LogoComponent from "./components/LogoComponent.vue";
 export default {
-  components: {},
+  components: {
+    LogoComponent,
+  },
 };
 </script>
 
-<style scoped>
-header {
-  text-align: center;
-  line-height: 1.5;
+<style>
+* {
+  box-sizing: border-box;
+  padding: 0;
 }
 
-#logo {
-  margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-}
-</style>
-
-<style scoped>
 header {
   line-height: 1.5;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
 }
 
 @media (min-width: 1024px) {
