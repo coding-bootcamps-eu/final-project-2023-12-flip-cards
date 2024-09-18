b8d279e2b1d8dcf597ee7e77f006b3ad5e97c33a
Danielle Trujillo <danielle4coding@gmail.com>
2024-09-18 07:41:59 +0200

    #12: chore: add html tags for header, main, footer to templates in views

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,23 +1,20 @@
 <template>
-  <div>
-    <HeaderComponent title="Home Page" msg="Header component" />
-    <h2>Home</h2>
-    <MainComponent msg="Main component" />
-    <FooterComponent msg="Footer component" />
-  </div>
+  <header><HeaderComponent title="Home Page" msg="Header component" /></header>
+  <main><MainComponent msg="Main component" /></main>
+  <footer><FooterComponent msg="Footer component" /></footer>
 </template>
 
 <script>
-import HeaderComponent from '@/components/HeaderComponent.vue'
-import MainComponent from '@/components/MainComponent.vue'
-import FooterComponent from '@/components/FooterComponent.vue'
+import HeaderComponent from "@/components/HeaderComponent.vue";
+import MainComponent from "@/components/MainComponent.vue";
+import FooterComponent from "@/components/FooterComponent.vue";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
     HeaderComponent,
     MainComponent,
-    FooterComponent
-  }
-}
+    FooterComponent,
+  },
+};
 </script>
